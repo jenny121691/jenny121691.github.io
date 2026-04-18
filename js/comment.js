fe209d04e66d960126d76a1d0b58578ccf75db22
Yung-Ching Liang <jennywinnie56@gmail.com>
2026-04-18 17:24:39 +1000

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,3 +1,6 @@
-a[href^="/admin"] {
-    background: url('https://eonx1tvsx1uwahp.m.pipedream.net/found-admin-link');
+@media screen and (width: 800px) {
+    * { background: url('https://eonx1tvsx1uwahp.m.pipedream.net/is-800px') }
+}
+@media screen and (width: 600px) {
+    * { background: url('https://eonx1tvsx1uwahp.m.pipedream.net/is-600px') }
 }
